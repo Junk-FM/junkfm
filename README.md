c3e760718a7bc376b5b1346dada7c25b74606517
Chris Mochinski <cmochinski@gmail.com>
2025-02-22 17:36:53 -0600

    placeholder, dark mode

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Junk FM - Upcoming Site Update 2025
 
+React + Typescript + Vite<br />
+Mantine V6<br />
+
+Additional deps and libraries = _BRING IT!_ ...just update the above
+
+## BUILD STUFF
+
+Build progress can be viewed at [junkfm.netlify.app/](https://junkfm.netlify.app/). You can check out a feature branch from `main` or work on `main` directly. _Pushing changes while on `main` will automatically run the Netlify build and update the above URL after a few minutes_
+
+You can check the status of those builds at 
+
 For now, you can still check us out at [junkfm.com](https://junkfm.com).
 
-## Vite run/kill shortcuts (WIP)
+## VITE RUN/KILL SHORTCUTS
 
 ### TO RUN:
 
@@ -17,4 +28,4 @@ You can do a `COMMAND + c` in the terminal but, in case of future expansion or a
 
 1. In VSCode, run `CONTROL + OPTION + COMMAND + k` _(kills all pertinent processes)_
 
-#### 💡 I'll add some automation scripts later. Steal at will!
+##### 💡 I'll add some automation scripts later. Steal at will!
---
+++ eslint.config copy.js
@@ -0,0 +1,56 @@
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["dist",
+      "node_modules",
+      "coverage",
+    ],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/no-unknown-property": "error", // Enforce camelCase for JSX attributes
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'react/react-in-jsx-scope': 'off', // Vite takes care of importing React
+      'import/no-unresolved': 'off', // Defer to TS
+      'no-undef': 'off', // Defer to TS
+      'react/no-array-index-key': 'off', // TS can type-check array keys
+      'no-use-before-define': ['error', { functions: false }], // Allow function hoisting
+      'react/require-default-props': 'off', // TS performs strict null checking, not necessary.
+      'no-underscore-dangle': 'off', // Allow underscore in object keys
+      'no-new': 'off',
+      'no-nested-ternary': 'off',
+      'no-restricted-syntax': 'off', // We bundle all our code, so we don't need to worry about using newer JS syntax
+      'no-shadow': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+];
