3ecd3f76708811264c4ecce38c6f3a3c23b21413
Chris Mochinski <cmochinski@gmail.com>
2025-02-24 15:08:29 -0700

    imports working with icky path imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../@/components/ui/dialog';
 import './App.css';
 
 function App() {
@@ -14,9 +22,21 @@ function App() {
         width: '100vw',
       }}
     >
-      <h1 style={{ color: 'white' }}>Junk FM</h1>
+      <h1 style={{ color: 'white', fontSize: '2rem' }}>SHADCN TEST Y'ALL</h1>
       <h4 style={{ color: 'white' }}>This is it. The site is done.</h4>
       <h5 style={{ color: 'white' }}>JK. Coming soon.</h5>
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
